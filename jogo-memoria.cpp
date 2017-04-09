89ab9277358278d9a410d6ad8932c58baff168b8
edersonlrf <edersonluis.rf@gmail.com>
2017-04-09 11:42:07 -0300

    exibindo palpites

--- jogo-memoria.cpp
+++ jogo-memoria.cpp
@@ -1,13 +1,77 @@
 #include <stdio.h>
 #include <conio.c>
 
+exibe_cartas(int cartas[4][5], int reveladas[4][5], int palpite_linha[2], char palpite_coluna[2]) {
+	int i, j, cont, aux;
+	char colunas[5] = {'a','b','c','d','e'};
+	// exibir linha
+	for(i = 0; i < 4; i++) {
+		gotoxy(4, 6+(i*2));
+		printf("  %i  ",i+1);
+	}
+	// exibir coluna
+	for(j = 0; j < 5; j++) {
+		gotoxy(10+(j*5), 4);
+		printf("  %c  ",colunas[j]);
+	}
+	// exibir a matriz
+	for(i = 0; i < 4; i++) {
+		for(j = 0; j < 5; j++) {
+			aux = 1;
+			cont = 0;
+			while(cont < 2) {
+				if (palpite_linha[cont] == i && palpite_coluna[cont] == colunas[j]) {
+					gotoxy(9+(j*5), 5+(i*2));
+					printf("-----");
+					gotoxy(9+(j*5), 6+(i*2));
+					printf("|> %i ", cartas[i][j]);
+					gotoxy(9+(j*5), 7+(i*2));
+					printf("-----");
+					aux = 0;
+				}
+				cont++;
+			}
+			if (aux) {
+				if (reveladas[i][j] == 1) {
+					gotoxy(9+(j*5), 5+(i*2));
+					printf("-----");
+					gotoxy(9+(j*5), 6+(i*2));
+					printf("|  %i ", cartas[i][j]);
+					gotoxy(9+(j*5), 7+(i*2));
+					printf("-----");
+				} else {
+					gotoxy(9+(j*5), 5+(i*2));
+					printf("-----");
+					gotoxy(9+(j*5), 6+(i*2));
+					printf("|    ");
+					gotoxy(9+(j*5), 7+(i*2));
+					printf("-----");
+				}
+			}
+		}
+		gotoxy(9+(j*5), 5+(i*2));
+		printf("-");
+		gotoxy(9+(j*5), 6+(i*2));
+		printf("|");
+		gotoxy(9+(j*5), 7+(i*2));
+		printf("-");
+	}
+}
+
 main() {
 	char nome[20];
+
 	int cartas[4][5] = {
 		{1,2,3,4,5},
 		{6,7,8,9,10},
 		{1,2,3,4,5},
 		{6,7,8,9,10}
+	};
+	int reveladas[4][5] = {
+		{0,0,0,0,0},
+		{0,0,0,0,1},
+		{0,0,0,0,0},
+		{0,0,0,0,1}
 	};
 
 	clrscr();
@@ -24,41 +88,13 @@ main() {
 	gotoxy(4,2);
 	printf("Jogo da Memoria");
 	
-	int i, j;
-	char colunas[5] = {'A','B','C','D','E'};
-	// exibir linha
-	for(i = 0; i < 4; i++) {
-		gotoxy(4, 6+(i*2));
-		printf("  %i  ",i+1);
-	}
-	// exibir coluna
-	for(j = 0; j < 5; j++) {
-		gotoxy(10+(j*5), 4);
-		printf("  %c  ",colunas[j]);
-	}
-	// exibir a matriz
-	for(i = 0; i < 4; i++) {
-		for(j = 0; j < 5; j++) {
-			gotoxy(9+(j*5), 5+(i*2));
-			printf("-----");
-			gotoxy(9+(j*5), 6+(i*2));
-			printf("|    ");
-			gotoxy(9+(j*5), 7+(i*2));
-			printf("-----");
-		}
-		gotoxy(9+(j*5), 5+(i*2));
-		printf("-");
-		gotoxy(9+(j*5), 6+(i*2));
-		printf("|");
-		gotoxy(9+(j*5), 7+(i*2));
-		printf("-");
-	}
   
 	// coletar os dois palpites.
 	int palpite_linha[2];
 	char palpite_coluna[2];
+	exibe_cartas(cartas, reveladas, palpite_linha, palpite_coluna);
 	int cont = 0;
-	while(cont < 2) {		
+	while(cont < 2) {	
 		gotoxy(4,18+(cont*4));
 		printf("Digite uma linha: ");
 		scanf(" %i",&palpite_linha[cont]);
@@ -66,12 +102,11 @@ main() {
 		gotoxy(4,20+(cont*4));
 		printf("Digite uma coluna: ");
 		scanf(" %c",&palpite_coluna[cont]);
+		
+		exibe_cartas(cartas, reveladas, palpite_linha, palpite_coluna);
 
 		cont++;
 	}
-	
-	gotoxy(1,1);
-	printf("%i %i %c %c",palpite_linha[0],palpite_linha[1],palpite_coluna[0],palpite_coluna[1]);
 
 	getch();
 }
